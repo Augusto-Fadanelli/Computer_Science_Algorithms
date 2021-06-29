138a7a8e37916518fbfe9ebcddc0b7ce0d625009
Augusto Fadanelli de Sousa <augustofadanelli.work@gmail.com>
2021-06-29 19:41:02 -0300

    Create insertionSort.h

---
+++ Sorting Algorithms/insertionSort.h
@@ -0,0 +1,24 @@
+#ifndef _INSERTIONSORT_H
+#define _INSERTIONSORT_H
+
+#include <stdio.h>
+
+void insertionSort(int *vet, int n){
+
+    int aux; //Variable used to exchange integers in vector positions
+    int j;
+
+    for(int i=0; i < n; i++){
+        aux = vet[i];
+        j = i-1;
+
+        while(j>=0 && vet[j] > aux){ //Invert the logical operator to change the order (> ascending; < descending)
+            vet[j+1] = vet[j];
+            j--;
+        }
+
+        vet[j+1] = aux;
+    }
+}
+
+#endif
